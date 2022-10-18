ff70873265ec10158d82bf111e682f8479a05b86
TodorovManuel <alumno24.todorov.manuel@ipm.edu.ar>
2022-10-18 16:56:25 -0300

    j

---
+++ elementos/footer.js
@@ -0,0 +1,21 @@
+let footerContainer = document.getElementById('footer')
+
+footerContainer.innerHTML=`
+<div class="teller">
+      <p class="title">Suscribite a nuestro Newsteller</p>
+      <form action="">
+        <input placeholder="leomessi@gmail.com" type="email">
+        <button type="submit">Suscribirme</button>
+      </form>
+    </div>
+    <div class="redes">
+      <a target="_blank" href="https://www.instragram.com"><img class="icon" src="./imagenes/instagram(1).png" alt=""></a>
+      <a target="_blank" href="https://www.twitter.com"><img class="icon" src="./imagenes/gorjeo.png" alt=""></a>
+      <a target="_blank" href="https://www.tiktok.com"><img class="icon" src="./imagenes/tik-tok.png" alt=""></a>
+    </div>
+    <div class="quienes">
+      <p class="footerP"><b>Manuel Todorov:</b> Desarrollo web</p>
+      <p class="footerP"><b>Nahuel Genovesi:</b> Diseño y edición de imagenes</p>
+      <p class="footerP"><b>Ian simoes:</b> Apoyo moral</p>
+    </div>
+`
